4e97da75336c458abd60912795e3eb2ac7165d50
Kociński Tomasz <Tomasz.Kocinski@coi.gov.pl>
2017-07-18 09:14:26 +0200

    first

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,24 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tkocinski on 17.07.2017.
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
